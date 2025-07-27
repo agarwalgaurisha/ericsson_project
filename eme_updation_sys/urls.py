982971d6d5f5bb8cd05806a5cc24a0389b2eb503
gaurisha agarwal <agarwalgaurisha123@gmail.com>
2025-07-27 17:31:06 +0530

    4 updation

--- eme_updation_sys/urls.py
+++ eme_updation_sys/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from eme.views import home
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('eme/', include('eme.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
